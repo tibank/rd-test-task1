09faf3e8e801e633f7432c1a4c499e96056ad5fc
Volodymyr Lysenko <tibank2071@gmail.com>
2022-09-12 12:57:53 +0300

    feat:  create header notes list

--- src/modules/helpers/createTagElememt.js
+++ src/modules/helpers/createTagElememt.js
@@ -1,14 +1,9 @@
-export default function createTagElem(tag, content = '', isText = true, classList = [], attrList = {}) {
+export default function createTagElem(tag, content = '', isText = true, classNames = [], attrList = {}) {
   let elem = document.createElement(tag);
 
   isText ? (elem.textContent = content) : (elem.innerHTML = content);
-  // if (isText) {
-  //   elem.textContent = content;
-  // } else {
-  //   elem.innerHTML = content;
-  // }
 
-  classList.forEach((itemClass) => elem.classList.add(itemClass));
+  elem.classList.add(...classNames);
   for (const [key, value] of Object.entries(attrList)) {
     elem.setAttribute(key, value);
   }
